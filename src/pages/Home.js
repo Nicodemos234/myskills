a2462f369dff80a398368893a77c3839c58bb485
nicodemos234 <nicodemos54324@gmail.com>
2021-07-16 13:36:09 -0300

    Home Structure and Styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity
+} from 'react-native';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <Text>Ignite</Text>
+      <Text style={styles.title}>Welcome, Nicodemos</Text>
+      <TextInput style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity 
+      activeOpacity={.7}
+      style={styles.button}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      
+      <Text style={[styles.title, {marginTop: 50}]}>
+        My skills
+      </Text>
     </View>
   )
 }
@@ -13,7 +32,33 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#121015',
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: '#1F1e25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
   }
 });
